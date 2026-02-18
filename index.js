ea2fe78e1f3c672dfd20a52286476de8e97bdbc0
VaibhavBhardwaj23 <majesty.v2507@gmail.com>
2026-02-18 23:47:28 +0530

    Final Stages

--- index.js
+++ index.js
@@ -16,6 +16,66 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+/* BUTTONS (can exist multiple times) */
+const appStoreBtns = document.querySelectorAll(".appStore");
+const playStoreBtns = document.querySelectorAll(".playStore");
+
+/* OVERLAYS */
+const overLayIphone = document.querySelector(".overLayIphone");
+const overLayAndroid = document.querySelector(".overLayAndroid");
+
+/* BOXES */
+const boxI = document.querySelector(".iphoneQRBox");
+const boxA = document.querySelector(".androidQRBox");
 
 
+/* OPEN POPUPS */
+appStoreBtns.forEach(btn=>{
+  btn.addEventListener("click", ()=>{
+    overLayIphone.classList.add("show");
+    document.body.classList.add("noScroll");
+  });
+});
+
+playStoreBtns.forEach(btn=>{
+  btn.addEventListener("click", ()=>{
+    overLayAndroid.classList.add("show");
+    document.body.classList.add("noScroll");
+  });
+});
+
+
+/* CLOSE ON OVERLAY CLICK */
+overLayIphone.addEventListener("click", ()=>{
+  overLayIphone.classList.remove("show");
+  document.body.classList.remove("noScroll");
+});
+
+overLayAndroid.addEventListener("click", ()=>{
+  overLayAndroid.classList.remove("show");
+  document.body.classList.remove("noScroll");
+});
+
+
+/* PREVENT CLOSE WHEN CLICKING BOX */
+boxI.addEventListener("click", e => e.stopPropagation());
+boxA.addEventListener("click", e => e.stopPropagation());
+
+
+// CTA Functionality
+document.querySelector(".downloadApp").addEventListener("click", function(e){
+  e.preventDefault();
+
+  const ua = navigator.userAgent.toLowerCase();
+
+  const isAndroid = ua.includes("android");
+  const isIOS = /iphone|ipad|ipod/.test(ua);
+
+  if(isAndroid){
+    window.location.href = "https://play.google.com/store/apps/details?id=YOUR.APP";
+  }
+  else if(isIOS){
+    window.location.href = "https://apps.apple.com/in/app/jumbo-shop-free-games-win/id6475365928";
+  }
+});
 
